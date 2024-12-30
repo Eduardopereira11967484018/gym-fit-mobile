1659d5d1bbde68545bc3bc0b54e3118a6be9e91e
Eduardo Pereira <eduardopereira.lima@yahoo.com>
2024-12-30 13:36:35 -0300

    inicio do arquivo mobile

---
+++ app/index.tsx
@@ -0,0 +1,76 @@
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
+import { Colors } from '../constants/Colors';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/images/logo.png')}
+        style={styles.logo}
+      />
+
+      <Text style={styles.title}>
+        Gym<Text style={{ color: Colors.green }}>Fit</Text>
+      </Text>
+
+      <Text style={styles.text}>
+      Start your journey with a personal trainer powered by artificial intelligence
+      </Text>
+
+      
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Get started</Text>
+        </Pressable>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 24,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: Colors.green,
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  text: {
+    fontSize: 16,
+    color: Colors.white,
+    width: 240,
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: Colors.blue,
+    width: '80%',
+    height: 48,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 24,
+    elevation: 4, // Para sombra no Android
+    shadowColor: '#000', // Para sombra no iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+  buttonText: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
---
+++ constants/Colors.ts
@@ -0,0 +1,9 @@
+
+export const Colors = {
+    background: 'rgb(231, 100, 56)',
+    white: ' #FFFFFF',
+    green: '#1EB500',
+    orange: '#F79600',
+    blue: '#009AD6',
+    black: '#000'
+  };
