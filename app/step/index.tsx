ad8932c70560a4f3e641606922406157aa64a818
Eduardo Pereira <eduardopereira.lima@yahoo.com>
2024-12-30 16:14:21 -0300

    inplataçao de shema via zod

--- app/step/index.tsx
+++ app/step/index.tsx
@@ -1,133 +1,135 @@
 import { 
-    View, 
-    Text,  
-    StyleSheet, 
-    Pressable, 
-    ScrollView
-   } from 'react-native'
-//styles
-  import { Colors } from '../../constants/Colors'
+  View, 
+  Text,  
+  StyleSheet, 
+  Pressable, 
+  ScrollView
+ } from 'react-native'
+import { Colors } from '../../constants/Colors'
+import { Header } from '../../componets/header/index'
+import { Input } from '../../componets/input/index'
 
-  import { Header } from '../../components/header'
-  import { Input } from '../../componets/input'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from  'react-hook-form'
+import { router } from 'expo-router'
+//import { useDataStore } from '../../store/data'
+
+
+const schema = z.object({
+  name: z.string().min(1, { message: "O nome é obrigatório"}),
+  weight: z.string().min(1, { message: "O peso é obrigatório"}),
+  age: z.string().min(1, { message: "A idade é obrigatória"}),
+  height: z.string().min(1, { message: "A altura é obrigatória"}),
+})
+
+type FormData = z.infer<typeof schema>
+
+export default function Step(){
   
-  import { z } from 'zod'
-  import { zodResolver } from '@hookform/resolvers/zod'
-  import { useForm } from  'react-hook-form'
-  import { router } from 'expo-router'
-  import { useDataStore } from '../../store/data'
-  
-  
-  const schema = z.object({
-    name: z.string().min(1, { message: "O nome é obrigatório"}),
-    weight: z.string().min(1, { message: "O peso é obrigatório"}),
-    age: z.string().min(1, { message: "A idade é obrigatória"}),
-    height: z.string().min(1, { message: "A altura é obrigatória"}),
+  const { control, handleSubmit, formState: { errors, isValid } } = useForm<FormData>({
+    resolver: zodResolver(schema)
   })
-  
-  type FormData = z.infer<typeof schema>
-  
-  export default function Step(){
+
+  const setPageOne = useDataStore(state => state.setPageOne)
+
+
+  function handleCreate(data: FormData){
+    console.log("PASSANDO DADOS DA PAGINA 1");
     
-    const { control, handleSubmit, formState: { errors, isValid } } = useForm<FormData>({
-      resolver: zodResolver(schema)
+    setPageOne({
+      name: data.name,
+      weight: data.weight,
+      age: data.age,
+      height: data.height
     })
-  
-    const setPageOne = useDataStore(state => state.setPageOne)
-  
-  
-    function handleCreate(data: FormData){
-      console.log("PASSANDO DADOS DA PAGINA 1");
-      
-      setPageOne({
-        name: data.name,
-        weight: data.weight,
-        age: data.age,
-        height: data.height
-      })
-  
-      router.push("/create")
-    }
-  
-    return(
-      <View style={styles.container}>
-        <Header
-          step='Passo 1'
-          title='Vamos começar'
-        />
-  
-        <ScrollView style={styles.content}>
-          <Text style={styles.label}>Nome:</Text>
-          <Input
-            name="name"
-            control={control}
-            placeholder="Digite seu nome..."
-            error={errors.name?.message}
-            keyboardType="default"
-          />
-          
-          <Text style={styles.label}>Seu peso atual:</Text>
-          <Input
-            name="weight"
-            control={control}
-            placeholder="Ex: 75"
-            error={errors.weight?.message}
-            keyboardType="numeric"
-          />     
-          
-          <Text style={styles.label}>Sua altura atual:</Text>
-          <Input
-            name="height"
-            control={control}
-            placeholder="Ex: 1.90"
-            error={errors.height?.message}
-            keyboardType="numeric"
-          />     
-          
-          <Text style={styles.label}>Sua idade atual:</Text>
-          <Input
-            name="age"
-            control={control}
-            placeholder="Ex: 24"
-            error={errors.age?.message}
-            keyboardType="numeric"
-          />
-  
-          <Pressable style={styles.button} onPress={handleSubmit(handleCreate)}>
-            <Text style={styles.buttonText}>Avançar</Text>
-          </Pressable>     
-  
-        </ScrollView>
-  
-      </View>
-    )
+
+   router.push("/create/index")
   }
-  
-  const styles = StyleSheet.create({
-    container:{
-      flex:1,
-      backgroundColor: colors.background
-    },
-    content:{
-      paddingLeft: 16,
-      paddingRight: 16,
-    },
-    label:{
-     fontSize: 16,
-     color: colors.white,
-     fontWeight: 'bold',
-     marginBottom: 8, 
-    },
-    button:{
-      backgroundColor: colors.blue,
-      height: 44,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 4,
-    },
-    buttonText:{
-      color: colors.white,
-      fontSize: 16,
-      fontWeight: 'bold'
-    }
-  })
+
+  return(
+    <View style={styles.container}>
+      <Header
+        step='Passo 1'
+        title='Vamos começar'
+      />
+
+      <ScrollView style={styles.content}>
+        <Text style={styles.label}>Nome:</Text>
+        <Input
+          name="name"
+          control={control}
+          placeholder="Digite seu nome..."
+          error={errors.name?.message}
+          keyboardType="default"
+        />
+        
+        <Text style={styles.label}>Seu peso atual:</Text>
+        <Input
+          name="weight"
+          control={control}
+          placeholder="Ex: 75"
+          error={errors.weight?.message}
+          keyboardType="numeric"
+        />     
+        
+        <Text style={styles.label}>Sua altura atual:</Text>
+        <Input
+          name="height"
+          control={control}
+          placeholder="Ex: 1.90"
+          error={errors.height?.message}
+          keyboardType="numeric"
+        />     
+        
+        <Text style={styles.label}>Sua idade atual:</Text>
+        <Input
+          name="age"
+          control={control}
+          placeholder="Ex: 24"
+          error={errors.age?.message}
+          keyboardType="numeric"
+        />
+
+        <Pressable style={styles.button} onPress={handleSubmit(handleCreate)}>
+          <Text style={styles.buttonText}>Avançar</Text>
+        </Pressable>     
+
+      </ScrollView>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: Colors.background
+  },
+  content:{
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  label:{
+   fontSize: 16,
+   color: Colors.white,
+   fontWeight: 'bold',
+   marginBottom: 8, 
+  },
+  button:{
+    backgroundColor: Colors.blue,
+    height: 44,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+  buttonText:{
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+})
+
+function useDataStore(arg0: (state: any) => any) {
+  throw new Error('Function not implemented.')
+}
--- componets/input/index.tsx
+++ componets/input/index.tsx
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
   },
   input:{
     height: 44,
-    backgroundColor: colors.white,
+    backgroundColor: Colors.white,
     paddingHorizontal: 10,
     borderRadius: 4,
   },
